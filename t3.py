b1bd04ed94f38b6832f75b4c36165aa9bcb236fd
abhinav-766 <abhinav_2001220310002@srmcem.ac.in>
2023-07-17 21:24:40 +0530

    t3

---
+++ t3.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+speed('fastest')
+colors =['red','yellow','blue','green','purple']
+bgcolor('black')
+pensize(2)
+
+for i in range(100):
+    #print(i%4,colors[i%4])
+    pencolor(colors[i%4])
+    fd(i*2)
+    lt(60)
+    circle(i*2,720)
+
+mainloop()
+ 
